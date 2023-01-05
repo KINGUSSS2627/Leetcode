dd1f324219945576aac93e252404e245600f9456
SHRESHTH GUPTA <107746269+KINGUSSS2627@users.noreply.github.com>
2023-01-06 00:06:02 +0530

    Add files via upload
    https://leetcode.com/problems/delete-columns-to-make-sorted/description/

---
+++ Daily/Delete_Coloums_to_make_sorted.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minDeletionSize(vector<string> &strs)
+    {
+        int delete_count = 0;
+        int row = strs.size();
+        int col = strs[0].size();
+
+        for (int j = 0; j < col; j++)
+        {
+            for (int i = 0; i < row - 1; i++)
+            {
+                if (strs[i][j] > strs[i + 1][j])
+                {
+                    delete_count++;
+                    break;
+                }
+            }
+        }
+        return delete_count;
+    }
+};
+int main()
+{
+    vector<string> s = {"cba", "daf", "ghi"};
+    Solution s1;
+    cout << s1.minDeletionSize(s);
+    return 0;
+}
